d7ca49c8b6740ae50b0a98a89851b67e7b78d498
ControlNet <smczx@hotmail.com>
2024-01-07 23:12:04 +1100

    Fix wget download huggingface model

--- download_models.sh
+++ download_models.sh
@@ -6,7 +6,7 @@ PRETRAINED_MODELS_PATH=./pretrained_models
 # GLIP model
 mkdir -p $PRETRAINED_MODELS_PATH/GLIP/checkpoints
 mkdir -p $PRETRAINED_MODELS_PATH/GLIP/configs
-wget -nc -P $PRETRAINED_MODELS_PATH/GLIP/checkpoints https://huggingface.co/GLIPModel/GLIP/blob/main/glip_large_model.pth
+wget -nc -P $PRETRAINED_MODELS_PATH/GLIP/checkpoints https://huggingface.co/GLIPModel/GLIP/resolve/main/glip_large_model.pth
 wget -nc -P $PRETRAINED_MODELS_PATH/GLIP/configs https://raw.githubusercontent.com/microsoft/GLIP/main/configs/pretrain/glip_Swin_L.yaml
 
 # X-VLM model
